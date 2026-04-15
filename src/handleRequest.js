6dfb5786ce16260efbf1856d3603364990927ad3
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-04-15 12:57:17 -0300

    Create handleRequest.js

---
+++ src/handleRequest.js
@@ -0,0 +1,24 @@
+import AMI from './AMI.js';
+
+export default async function handleRequest(req) {
+  const body = await req.body;
+
+  if (!body) throw new Error('Missing request body');
+
+  if (!body.host) throw new Error('Missing gateway host');
+
+  if (!body.port) throw new Error('Missing gateway port');
+
+  if (!body.user) throw new Error('Missing gateway user');
+
+  if (!body.password) throw new Error('Missing gateway password');
+
+  if (!body.channel) throw new Error('Missing channel number');
+
+  if (!body.number) throw new Error('Missing recipient number');
+
+  if (!body.message) throw new Error('Missing message message');
+
+  const result = await AMI.connectAndSendSMS(body.host, body.port, body.user, body.password, body.channel, body.number, body.message);
+  return result;
+}
